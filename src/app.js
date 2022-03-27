6cac99206208e144188346ce82fea2dd649addd0
minterger <minterger@gmail.com>
2022-03-26 23:20:21 -0300

    add cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const connectDB = require("./database");
+const cors = require("cors");
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.set("port", process.env.PORT || 3000);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use(morgan("dev"));
 
 app.use("/auth", require("./routes/auth.routes"));
