785a06b83846a47112018664e3017c92bb8367bc
Minterger <minterger@gmail.com>
2025-04-14 23:17:07 -0300

    update(database): connection

--- src/database.js
+++ src/database.js
@@ -1,13 +1,18 @@
 const mongoose = require("mongoose");
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("connected to mongodb");
-  } catch (error) {
-    console.log("error connecting to mongodb", error);
-    process.exit(1);
-  }
+mongoose.set("strictQuery", true); // Configura strictQuery
+
+const connectDB = () => {
+  mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  mongoose.connection.on("error", (error) => {
+    console.log("MongoDB connection error:", error);
+  });
+  mongoose.connection.on("connected", () => {
+    console.log("MongoDB connected");
+  });
 };
 
 module.exports = connectDB;
